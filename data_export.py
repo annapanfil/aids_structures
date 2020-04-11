ef4a5586e832514c17acbb0d4d0266be080139a7
MichalRasinski <michal.rasinski@icloud.com>
2020-04-11 13:36:34 +0200

    tree height data export

--- data_export.py
+++ data_export.py
@@ -17,6 +17,23 @@ def export_data_to_file(data: int, filename:str, function, head = 0):
     file.close()
     return result
 
-
+def export_data_h_tree(filename, probes):
+    file = open(filename, "w")
+    file.write("{}\t{}\t{}\n".format("n","h_bst","h_avl"))
+    for n in range(1000, 10000, 200):
+        h1=0
+        h2=0
+        for a in range(probes):
+            data=generate(n)
+            root_bst=create_bst(data)
+            lista =[]
+            in_order_list(root_bst,lista)
+            root_avl=create_bst_from_io_list(lista)
+            h1+=find_height(root_bst)
+            h2+=find_height(root_avl)
+        h1/=probes
+        h2/=probes
+        file.write("{}\t{}\t{}\n".format(n, h1, h2))
+    file.close()
 if __name__ == "__main__":
     export_data_to_file(2,3,"data/wynik.txt", createList)
